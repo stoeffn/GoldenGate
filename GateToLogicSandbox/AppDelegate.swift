e7e90e998f1fdb441abde7142b8c9c1a3d3be8cd
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-03-23 18:13:10 +0100

    :tada: Generate macOS Sandbox game app

---
+++ GateToLogicSandbox/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GateToLogicSandbox
+//
+//  Created by Steffen Ryll on 23.03.18.
+//  Copyright © 2018 Steffen Ryll. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
