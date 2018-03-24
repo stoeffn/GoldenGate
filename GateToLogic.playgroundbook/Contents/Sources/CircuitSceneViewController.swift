7b1c1fdfa0ad598725b77b82ceececaf8d4cdf79
Steffen Ryll <13511737+stoeffn@users.noreply.github.com>
2018-03-24 15:16:02 +0100

    :lipstick: Add orthographic camera

--- GateToLogic.playgroundbook/Contents/Sources/CircuitSceneViewController.swift
+++ GateToLogic.playgroundbook/Contents/Sources/CircuitSceneViewController.swift
@@ -30,16 +30,37 @@ final class CircuitSceneViewController : NSObject {
         return circuit
     }()
 
-    private lazy var scene = SCNScene()
+    private lazy var scene: SCNScene = {
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(cameraNode)
+        return scene
+    }()
 
     private(set) lazy var view: SCNView = {
         let view = SCNView()
         view.scene = scene
         view.delegate = self
-        view.allowsCameraControl = true
+        view.pointOfView = cameraNode
         view.autoenablesDefaultLighting = true
         view.showsStatistics = true
         return view
+    }()
+
+    private lazy var camera: SCNCamera = {
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        camera.orthographicScale = 6
+        camera.wantsHDR = true
+        return camera
+    }()
+
+    private lazy var cameraNode: SCNNode = {
+        let node = SCNNode()
+        node.name = "Camera"
+        node.position = SCNVector3(-8, 8, 10)
+        node.look(at: SCNVector3(0, 0, 0))
+        node.camera = camera
+        return node
     }()
 
     private lazy var componentNodes = [GridPoint: SCNNode]()
@@ -49,7 +70,7 @@ extension CircuitSceneViewController {
     func didAddComponent(at position: GridPoint) {
         let boxGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0)
         let boxNode = SCNNode(geometry: boxGeometry)
-        boxNode.position = SCNVector3(x: CGFloat(position.x), y: CGFloat(position.y), z: 0)
+        boxNode.position = SCNVector3(position.x, 0, position.y)
         scene.rootNode.addChildNode(boxNode)
         componentNodes[position] = boxNode
     }
